56dc70ee091c0e0d37f404ba3d7528dfeca1a190
VSRohod <89645335+VSRohod@users.noreply.github.com>
2026-02-25 17:30:23 -0300

    caixa de mensagem

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
+app.config["SECRET_KEY"] = '05e914aab8da1ba9dd24335b'
 db.init_app(app)
 
 from mercado import routes
--- mercado/routes.py
+++ mercado/routes.py
@@ -1,6 +1,8 @@
 from mercado import app
-from flask import render_template
-from mercado.models import Item
+from flask import render_template, redirect, url_for, flash
+from mercado.models import Item, User
+from mercado.forms import CadastroForm
+from mercado import db
 
 @app.route('/')
 def page_home():
@@ -10,3 +12,21 @@ def page_home():
 def page_produto():
     itens=Item.query.all()
     return render_template("produtos.html",itens=itens)
+
+@app.route('/cadastro', methods=['GET','POST'])
+def page_cadastro():
+    form = CadastroForm()
+    if form.validate_on_submit():
+        usuario = User(
+            usuario = form.usuario.data,
+            email = form.email.data,
+            senha = form.senha1.data
+        )
+        db.session.add(usuario)
+        db.session.commit()
+        return redirect(url_for('page_produto'))
+
+    if form.errors != {}:
+        for err in form.errors.values():
+            flash(f'Erro ao cadastrar usuário {err}', category="danger")
+    return render_template("cadastro.html",form=form)
