047489fcbf6d185e93958e8a53772f18fb06a693
VSRohod <89645335+VSRohod@users.noreply.github.com>
2026-03-16 15:34:16 -0300

    ajustando render

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -11,4 +11,7 @@ app.config["SECRET_KEY"] = '05e914aab8da1ba9dd24335b'
 db.init_app(app)
 bcrypt = Bcrypt(app)
 login_manager.init_app(app)
+login_manager.login_view = 'page_login'
+login_manager.login_message='Por Favor, realize o login'
+login_manager.login_message_category = "info"
 from mercado import routes
--- mercado/routes.py
+++ mercado/routes.py
@@ -3,13 +3,19 @@ from flask import render_template, redirect, url_for, flash
 from mercado.models import Item, User
 from mercado.forms import CadastroForm , LoginForm
 from mercado import db
-from flask_login import login_user , logout_user
+from flask_login import login_user , logout_user, login_required
+import stripe
+import os
+from dotenv import load_dotenv
+
+stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
 @app.route('/')
 def page_home():
     return render_template("home.html")
 
 @app.route('/produtos')
+@login_required
 def page_produto():
     itens=Item.query.all()
     return render_template("produtos.html",itens=itens)
@@ -50,3 +56,29 @@ def page_logout():
     logout_user()
     flash("Você fez o logout", category="info")
     return redirect(url_for("page_home"))
+
+@app.route('/comprar/<int:item_id>')
+@login_required
+def comprar(item_id):
+
+    item = Item.query.get_or_404(item_id)
+
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price_data': {
+                'currency': 'brl',
+                'product_data': {
+                    'name': item.nome,
+                },
+                'unit_amount': item.preco * 100,  
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=url_for('page_produto', _external=True),
+        cancel_url=url_for('page_produto', _external=True),
+
+    )
+
+    return redirect (session.url)
